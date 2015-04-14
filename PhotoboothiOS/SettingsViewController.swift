fceb0789f29f9b153d12ff9c642b06b2fcf23610
Ryan Choi <ryan.choi@gmail.com>
2015-04-13 21:34:07 -0700

    logout button

--- PhotoboothiOS/SettingsViewController.swift
+++ PhotoboothiOS/SettingsViewController.swift
@@ -2,11 +2,24 @@ import UIKit
 import MobileCoreServices
 import PhotoboothiOS
 
+import TwitterKit
 
 class SettingsViewController: UIViewController {
 
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
+    }
+    
+    @IBAction func logOut(sender: AnyObject) {
+        
+        Twitter.sharedInstance().logOut()
+        
+        // ensure that presentViewController happens from the main thread/queue
+        dispatch_async(dispatch_get_main_queue(), {
+            let controller = self.storyboard!.instantiateViewControllerWithIdentifier("AuthViewController") as UIViewController
+            self.presentViewController(controller, animated: true, completion: nil)
+        });
+        
     }
     
 }
