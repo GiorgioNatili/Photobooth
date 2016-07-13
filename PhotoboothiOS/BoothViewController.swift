9b16939b3f5f56d7606c6c61641e968553393062
GiorgioNatili <g.natili@gnstudio.com>
2016-07-12 21:04:06 -0400

    Fix resize of navigationBar border

--- PhotoboothiOS/BoothViewController.swift
+++ PhotoboothiOS/BoothViewController.swift
@@ -19,20 +19,35 @@ class BoothViewController : UIViewController {
     
     func setupNav(enableBackButton : Bool, enableSettings : Bool){
         
-        // Change the border to blue
-        let navHeight = self.navigationController?.navigationBar.frame.height
-        let navWidth = self.navigationController?.navigationBar.frame.width
-        let navBorder = UIView(frame: CGRectMake(0, navHeight! - 2, navWidth!, 2))
-        navBorder.backgroundColor = UIColor(rgba: "#5EA9DD")
-        self.navigationController?.navigationBar.addSubview(navBorder)
+        if let navigationController = self.navigationController {
+            
+            // Get the navigationBar.frame sizes
+            let navHeight = navigationController.navigationBar.frame.height
+            let navWidth = navigationController.navigationBar.frame.width
+            
+            // Create a border
+            let navBorder = UIView(frame: CGRectMake(0, navHeight - 2, navWidth, 2))
+            navBorder.tag = 140
+            
+            // Change the border to blue
+            navBorder.backgroundColor = UIColor(rgba: "#5EA9DD")
+            navigationController.navigationBar.addSubview(navBorder)
+            
+        }
         
         // Append image to the navigation bar
-        let logoView = UIImageView(frame: CGRectMake(0, 0, 30, 30))
-        logoView.image = UIImage(named: "TwitterLogo")!.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
-        logoView.contentMode = UIViewContentMode.ScaleAspectFit
-        logoView.frame.origin.x = 10
-        logoView.frame.origin.y = 8
-        navbar.titleView = logoView
+        if let image = UIImage(named: "TwitterLogo") {
+        
+            let logoView = UIImageView(frame: CGRectMake(0, 0, 30, 30))
+        
+            logoView.image = image.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
+            logoView.contentMode = UIViewContentMode.ScaleAspectFit
+            logoView.frame.origin.x = 10
+            logoView.frame.origin.y = 8
+            
+            navbar.titleView = logoView
+            
+        }
         
         if (!enableBackButton){
             self.navigationItem.setHidesBackButton(true, animated: true)
@@ -45,6 +60,18 @@ class BoothViewController : UIViewController {
             self.navbar.titleView!.addGestureRecognizer(recognizer)
         }
 
+    }
+    
+    override func viewDidLayoutSubviews() {
+        
+        super.viewDidLayoutSubviews()
+        
+        if let navigationController = self.navigationController,
+                border = navigationController.navigationBar.viewWithTag(140) {
+        
+                    let bounds = navigationController.navigationBar.bounds;
+                    border.frame = CGRectMake(0, bounds.height - 2, bounds.width, 2)
+        }
     }
     
 }
---
+++ PhotoboothiOS/BorderUINavigationBar.swift
@@ -0,0 +1,21 @@
+//
+//  BorderUINavigationBar.swift
+//  Photobooth
+//
+//  Created by Natili, Giorgio on 7/12/16.
+//  Copyright © 2016 Matt Donnelly. All rights reserved.
+//
+
+import UIKit
+
+class BorderUINavigationBar: UINavigationBar {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
